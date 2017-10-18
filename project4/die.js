78b9e55773042aab56dfd484bdaede6153cfa416
Jay Wong <xiao.hk1997@gmail.com>
2017-10-18 09:57:51 -0500

    Fix the top/bottom shader bug -> clockwise vs counter-clockwise matters

--- project4/die.js
+++ project4/die.js
@@ -24,7 +24,7 @@ function Die(){"use strict"
     var triangles = []
 
     // Color variables
-    var light = v3.normalize([-1,-1,-1])
+    var light = v3.normalize([0,-1,-1])
     var dieColor = [240, 60, 90]
     var topColor = [192, 192, 192]
     var dieColorString = colorToString(dieColor)
@@ -117,6 +117,27 @@ function Die(){"use strict"
         context.closePath()
         context.fill()
         context.stroke()
+    }
+
+    function drawNormal(Tx){
+        context.strokeStyle = "red"
+        for(var i = 0; i < triangles.length; i++){
+            var cur = triangles[i]
+            var mid = [(cur[0][0] + cur[1][0] + cur[2][0]) / 3,
+                       (cur[0][1] + cur[1][1] + cur[2][1]) / 3,
+                       (cur[0][2] + cur[1][2] + cur[2][2]) / 3]
+            var trans = [m4.transformPoint(Tx, cur[0]),
+                         m4.transformPoint(Tx, cur[1]),
+                         m4.transformPoint(Tx, cur[2])]
+            var norm = v3.normalize(v3.cross(v3.subtract(trans[1], trans[0]),
+                                             v3.subtract(trans[2], trans[0])))
+            var longNorm = v3.mulScalar(norm, 100)
+            context.beginPath()
+            moveToTx(mid[0], mid[1], mid[2], Tx)
+            lineToTx(mid[0]+longNorm[0],mid[1]+longNorm[1],mid[2]+longNorm[2],Tx)
+            context.closePath()
+            context.stroke()
+        }
     }
 
     // Draw the axes
@@ -226,6 +247,9 @@ function Die(){"use strict"
         var points = []
 
         phi = 0
+        points.push([xt + xb * Math.sin(theta) * Math.cos(phi),
+                     yt + yb * Math.cos(theta),
+                     zt + zb * Math.sin(theta) * Math.sin(phi)])
         for (phi = (1/(numOfPoints/2)) * Math.PI; phi <= 2 * Math.PI;
              phi += (1/(numOfPoints/2)) * Math.PI){
             location = [xt + xb * Math.sin(theta) * Math.cos(phi),
@@ -236,15 +260,53 @@ function Die(){"use strict"
         return points
     }
 
-    function addTop(top){
-        var points = getCircleBottomPoints([0,100,0], 100, 16)
+    // Add pointers for the tapers on the top
+    // The order of drawing triangles matters
+    function addTop(top, center, radius, num, encoder){
+        var points = getCircleBottomPoints(center, radius, num)
+        var triangle
         // Stop before the last one
         for(var i = 0; i < points.length - 1; i++){
-            triangles.push([top, points[i], points[i+1], topColorString, 0.0])
+            triangle = [top, points[i], points[i+1], topColorString, 0.0]
+            triangles.push(encoder(triangle))
         }
         // Add the last one to close the plane
-        triangles.push([top, points[points.length-1], points[0],
+        triangle = [top, points[points.length-1], points[0], topColorString, 0.0]
+        triangles.push(encoder(triangle))
+    }
+
+    // Add triangles for the tapers on the bottom
+    // The order of drawing triangles matters
+    function addBottom(top, center, radius, num, encoder){
+        var points = getCircleBottomPoints(center, radius, num)
+        var triangle
+        // Stop before the last one
+        for(var i = 0; i < points.length - 1; i++){
+            triangles.push([top, points[i+1], points[i], topColorString, 0.0])
+        }
+        // Add the last one to close the plane
+        triangles.push([top, points[0], points[points.length-1],
                        topColorString, 0.0])
+    }
+
+    // Define different triangle encoders for different positions
+    function encodeTopTaper(triangle){
+        var points = triangle.slice(0,3)
+        console.log(points)
+        triangle[4] = (triangle[1][2] + triangle[2][2]) / 2
+        return triangle
+    }
+
+    function encodeUpperTaper(triangle){
+        triangle[4] = triangle[0][2]
+        return triangle
+    }
+
+    function addSpinTop(){
+        addTop([0,400,0], [0,100,0], 100, 16, encodeTopTaper)
+        addTop([0,150,0], [0,50,0], 300, 32, encodeUpperTaper)
+        //addBottom([0,-100,0], [0,50,0], 300, 32)
+        //addBottom([0,-450,0], [0,-50,0], 100, 16)
     }
 
     // Add shader to each triangles
@@ -265,17 +327,18 @@ function Die(){"use strict"
             var trans = [m4.transformPoint(Tx, cur[0]),
                          m4.transformPoint(Tx, cur[1]),
                          m4.transformPoint(Tx, cur[2])]
+            //trans = cur
             var norm = v3.normalize(v3.cross(v3.subtract(trans[1], trans[0]),
                                              v3.subtract(trans[2], trans[0])))
             var dColor = v3.mulScalar(color, localDiffuse * 
                                       (0.5 * (1 + v3.dot(norm, light))))
             triangles[i][3] = colorToString(v3.add(aColor, dColor))
-            console.log(triangles[i][3])
+            //console.log(triangles[i][3])
         }
     }
 
     // Encode triangles into their distance
-    function encodeTriangle(Tmodel_to_camera){
+    function encodeTriangleCube(Tmodel_to_camera){
         for(var i = 0; i < triangles.length; i++){
             // Compute the middle point of a triangle
             var cur = triangles[i]
@@ -347,17 +410,18 @@ function Die(){"use strict"
         var Tmodel_to_view = m4.multiply(Tmodel_to_world, Tworld_to_view)
 
         // Actually drawing
-        drawAxes(Tworld_to_view)
+        
         //initGeometry()
         //addCube([0,0,0], 200, dieColorString)
-        addTop([0,400,0])
-        addShader(Tmodel_to_camera, 0.1, 0.9)
-        encodeTriangle(Tmodel_to_camera)
+        addSpinTop()
+        addShader(Tmodel_to_world, 0.1, 0.9)
+        //encodeTriangleCube(Tmodel_to_camera)
         sortGeometry()
         drawGeometry(Tmodel_to_view)
+        //drawNormal(Tmodel_to_view)
         //drawAxes(Tworld_to_view)
         //getCircleBottomPoints(Tmodel_to_view)
-        
+        drawAxes(Tworld_to_view) 
         //drawGeometry(Tmodel_to_view)
     }
 
